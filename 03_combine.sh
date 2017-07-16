0a5d41073595d55cb6895586dbf1830188ceb8b2
Germaine Yong <germaine.yong@ucsf.edu>
2017-07-16 11:27:19 -0700

    Add line to README

--- 03_combine.sh
+++ 03_combine.sh
@@ -12,9 +12,10 @@ cat data/clean/country.cc.txt > $OUTPUT_FILE
 mv -v data/clean/country.cc.txt data/clean/country.cc.processed
 sleep 2
 
-cat data/clean/*.cc.txt 
+cat data/clean/*.cc.txt # this just prints out what you are about to append in
 sleep 2
-cat data/clean/*.cc.txt >> $OUTPUT_FILE
+cat data/clean/*.cc.txt >> $OUTPUT_FILE #">>" appends instead of overwrites ">"
 
-mv -v data/clean/country.cc.processed data/clean/country.cc.txt
+mv -v data/clean/country.cc.processed data/clean/country.cc.txt #move temporary file back to 
+original file
 
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 SWC Day 2 repository using gapminder data
 
 Code from a SWC workshop at UCSF on 20170715
+
+We can also add text from local machine using nano.
